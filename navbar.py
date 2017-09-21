5e5ed96a9d92b12113267e4b8c9bc34151788a86
Rafael Santos <rafael@r5t.private>
2017-09-21 14:39:53 -0400

    interface working

--- navbar.py
+++ navbar.py
@@ -1,42 +1,53 @@
 from PySide2 import QtWidgets
 from PySide2 import QtCore
 
-def navigate(frame_incr = 12, direction = 'f'):
-    frameTo = hou.frame()
-    if direction == 'f' or direction == 1:
-        frameTo += frame_incr
-    elif direction == 'r' or direction == 0:
-        frameTo -= frame_incr
-    hou.setFrame(frameTo)
-
-def nav_sff():
-    frame_incr = 12
-    navigate(frame_incr, 'f')
-
-def createInterface():
+class Navbar(QtWidgets.QWidget):
+    # create interface elements
+    s_incr_parm = s_incr_parm = QtWidgets.QLineEdit('12')
     l_rew_btn = QtWidgets.QPushButton('<<')
     s_rew_btn = QtWidgets.QPushButton('<')
-    s_incr_parm = QtWidgets.QLineEdit('12')
-    s_incr_parm.setPlaceholderText('12')
     l_incr_parm = QtWidgets.QLineEdit('24')
-    l_incr_parm.setPlaceholderText('24')
-    s_incr_parm.setGeometry(QtCore.QRect(0, 0, 10, 20))
-    l_incr_parm.setGeometry(QtCore.QRect(0, 0, 10, 20))
     s_ff_btn = QtWidgets.QPushButton('>')
     l_ff_btn = QtWidgets.QPushButton('>>')
-
-    root_widget = QtWidgets.QWidget()
-    layout = QtWidgets.QHBoxLayout()
-    layout.addStretch(1)
-    layout.addWidget(l_rew_btn)
-    layout.addWidget(s_rew_btn)
-    layout.addWidget(s_incr_parm)
-    layout.addWidget(l_incr_parm)
-    layout.addWidget(s_ff_btn)
-    layout.addWidget(l_ff_btn)
-    layout.addStretch(1)
-    root_widget.setLayout(layout)
-
-    s_ff_btn.clicked.connect(nav_sff)
     
+    # class initializer
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        
+        self.setProperty("houdiniStyle", True)
+
+        self.s_incr_parm.setPlaceholderText('12')
+        self.s_incr_parm.setGeometry(QtCore.QRect(0, 0, 10, 20))
+        self.l_incr_parm.setPlaceholderText('24')
+        self.l_incr_parm.setGeometry(QtCore.QRect(0, 0, 10, 20))
+        self.s_ff_btn.clicked.connect(self.nav_sff)
+
+        layout = QtWidgets.QHBoxLayout()
+        layout.addStretch(1)
+        layout.addWidget(self.l_rew_btn)
+        layout.addWidget(self.s_rew_btn)
+        layout.addWidget(self.s_incr_parm)
+        layout.addWidget(self.l_incr_parm)
+        layout.addWidget(self.s_ff_btn)
+        layout.addWidget(self.l_ff_btn)
+        layout.addStretch(1)
+
+        self.setLayout(layout)
+
+
+    # created methods
+    def navigate(self, frame_incr = 12, direction = 'f'):
+        frameTo = hou.frame()
+        if direction == 'f' or direction == 1:
+            frameTo += frame_incr
+        elif direction == 'r' or direction == 0:
+            frameTo -= frame_incr
+        hou.setFrame(frameTo)
+
+    def nav_sff(self):
+        frame_incr = int(self.s_incr_parm.text())
+        self.navigate(frame_incr, 'f')
+
+def createInterface():
+    root_widget = Navbar() 
     return root_widget
